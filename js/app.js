c9dee70fea4729b9b9c5fcadb8182268a17d7ef7
oalkabouss <omaralkabous@gmail.com>
2019-02-20 21:49:59 +0100

    Migration to AngularJs

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
 (function () {
-    var app = new Vue({
-        el: '#app',
-        data: {
-            result: 0,
-            GroundSpeed : 0,
-            Δz:0,
-            Vz:0,
-        },
-        methods: {
-            calculate: function () {
-                this.result = (this.GroundSpeed * this.Δz)/(60*this.Vz);
-            }
-        }
-    })
-})()
+    "use strict";
+    angular.module("todApp", []);
+})();
---
+++ js/todController.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict";
+    class todController {
+
+        constructor() {
+            "ngInject";
+            this.GroundSpeed = 0;
+            this.DeltaZ = 0;
+            this.VitessV = 0;
+            this.result =0;
+        }
+
+        Calculate = () =>{
+            if(this.todFrom.$valid)
+            this.result =(this.GroundSpeed * this.DeltaZ) / (60*this.VitessV);
+        }
+    }
+
+    angular.module("todApp").controller('todController',todController);
+})()
